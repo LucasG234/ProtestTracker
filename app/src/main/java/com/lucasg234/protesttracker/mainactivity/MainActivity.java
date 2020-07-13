c04a6b20c9406277d911849d137fc71cc9822bfe
LucasG234 <lucasgates234@gmail.com>
2020-07-13 14:44:28 -0500

    Added navigation between fragments

--- app/src/main/java/com/lucasg234/protesttracker/mainactivity/MainActivity.java
+++ app/src/main/java/com/lucasg234/protesttracker/mainactivity/MainActivity.java
@@ -1,16 +1,60 @@
 package com.lucasg234.protesttracker.mainactivity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lucasg234.protesttracker.R;
+import com.lucasg234.protesttracker.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+
+        mBinding.mainBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch(item.getItemId()) {
+                    case R.id.bottomNavigationFeed:
+                        fragment = FeedFragment.newInstance("", "");
+                        break;
+                    case R.id.bottomNavigationCompose:
+                        fragment = ComposeFragment.newInstance("", "");
+                        break;
+                    case R.id.bottomNavigationMap:
+                        fragment = MapFragment.newInstance("", "");
+                        break;
+                    case R.id.bottomNavigationSettings:
+                        fragment = SettingsFragment.newInstance("", "");
+                        break;
+                    default:
+                        Log.i(TAG, "Bottom Navigation View selected unknown icon : " + item.toString());
+                        return false;
+                }
+                fragmentManager.beginTransaction().replace(R.id.fragmentHolder, fragment).commit();
+                return true;
+            }
+        });
+
+        // Set default selection
+        mBinding.mainBottomNavigation.setSelectedItemId(R.id.bottomNavigationFeed);
     }
 }
