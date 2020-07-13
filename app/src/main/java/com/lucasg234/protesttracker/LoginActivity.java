e714a51f82420800255910c0906c18101813c503
LucasG234 <lucasgates234@gmail.com>
2020-07-13 12:14:26 -0500

    Added parse integration

--- app/src/main/java/com/lucasg234/protesttracker/LoginActivity.java
+++ app/src/main/java/com/lucasg234/protesttracker/LoginActivity.java
@@ -3,12 +3,35 @@ package com.lucasg234.protesttracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.lucasg234.protesttracker.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private ActivityLoginBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        mBinding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.loginSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "Login button clicked");
+                String username = mBinding.loginUsernameText.getText().toString();
+                String password = mBinding.loginPasswordText.getText().toString();
+                loginUser(username, password);
+            }
+        });
+    }
+
+    private void loginUser(String username, String password) {
     }
 }
