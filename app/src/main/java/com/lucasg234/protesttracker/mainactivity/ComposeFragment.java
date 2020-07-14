e795ed93d9becead6d4aaf5473aa318520fe78cc
LucasG234 <lucasgates234@gmail.com>
2020-07-14 15:45:18 -0500

    Users can now post photos from the ComposeActivity

--- app/src/main/java/com/lucasg234/protesttracker/mainactivity/ComposeFragment.java
+++ app/src/main/java/com/lucasg234/protesttracker/mainactivity/ComposeFragment.java
@@ -3,6 +3,8 @@ package com.lucasg234.protesttracker.mainactivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -23,9 +25,11 @@ import com.lucasg234.protesttracker.databinding.FragmentComposeBinding;
 import com.lucasg234.protesttracker.models.Post;
 import com.lucasg234.protesttracker.models.User;
 import com.parse.ParseException;
+import com.parse.ParseFile;
 import com.parse.SaveCallback;
 
 import java.io.File;
+import java.io.IOException;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -90,12 +94,48 @@ public class ComposeFragment extends Fragment {
         configureTempImageStorage();
     }
 
+    // Used to receive photos after camera or gallery usage
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode != RESULT_OK) {
+            Toast.makeText(getContext(), getString(R.string.error_receive_image), Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        switch(requestCode) {
+            case ACTIVITY_REQUEST_CODE_CAMERA:
+                Log.i(TAG, "received photo");
+                // Does necessary resizing and rotations
+                Bitmap takenImage = receiveImage();
+                // Load the taken image into the preview space
+                mBinding.composeImagePreview.setImageBitmap(takenImage);
+                break;
+        }
+    }
+
+    private void configureTempImageStorage() {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Create the file target for camera taken images based on the constant file name
+        mTempImageStorage = new File(mediaStorageDir.getPath() + File.separator + TEMP_PHOTO_NAME);
+    }
+
 
     // Constructs Post object and saves it to the Parse server
     private void savePost() {
         Post post = new Post();
         post.setText(mBinding.composeEditText.getText().toString());
         post.setAuthor((User) User.getCurrentUser());
+        // The current image will be stored within the the temp image storage
+        post.setImage(new ParseFile(mTempImageStorage));
         post.saveInBackground(new SaveCallback() {
             @Override
             public void done(ParseException e) {
@@ -107,6 +147,7 @@ public class ComposeFragment extends Fragment {
 
                 Log.i(TAG, "Saved post successfully");
                 mBinding.composeEditText.setText("");
+                mBinding.composeImagePreview.setImageBitmap(null);
             }
         });
     }
@@ -127,36 +168,51 @@ public class ComposeFragment extends Fragment {
         }
     }
 
-    private void configureTempImageStorage() {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+    private Bitmap receiveImage() {
+        // Create and configure BitmapFactory
+        BitmapFactory.Options bounds = new BitmapFactory.Options();
+        bounds.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mTempImageStorage.getAbsolutePath(), bounds);
+        BitmapFactory.Options opts = new BitmapFactory.Options();
 
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(TAG, "failed to create directory");
+        // Decode the image in temp storage to a bitmap
+        Bitmap imageBitmap = BitmapFactory.decodeFile(mTempImageStorage.getAbsolutePath(), opts);
+
+        // Attempt to read EXIF Data about the image
+        ExifInterface exif = null;
+        try {
+            exif = new ExifInterface(mTempImageStorage.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Error getting EXIF data", e);
+            // If there is no EXIF data available, return the original Bitmap
+            return imageBitmap;
         }
 
-        // Create the file target for camera taken images based on the constant file name
-        mTempImageStorage = new File(mediaStorageDir.getPath() + File.separator + TEMP_PHOTO_NAME);
-    }
-
-    // Used to receive photos after camera or gallery usage
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode != RESULT_OK) {
-            Toast.makeText(getContext(), getString(R.string.error_receive_image), Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        switch(requestCode) {
-            case ACTIVITY_REQUEST_CODE_CAMERA:
-                Log.i(TAG, "received photo");
-                Bitmap takenImage = BitmapFactory.decodeFile(mTempImageStorage.getAbsolutePath());
-                // Load the taken image into the preview space
-                mBinding.composeImagePreview.setImageBitmap(takenImage);
+        // Find the correct orientation for the image from the EXIF data
+        String orientString = exif.getAttribute(ExifInterface.TAG_ORIENTATION);
+        int orientation = orientString != null ? Integer.parseInt(orientString) : ExifInterface.ORIENTATION_NORMAL;
+        int rotationAngle;
+        switch(orientation) {
+            default:
+                rotationAngle = 0;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotationAngle = 90;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotationAngle = 180;
+                break;
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotationAngle = 270;
                 break;
         }
+
+        // Rotate the Bitmap
+        Matrix matrix = new Matrix();
+        matrix.setRotate(rotationAngle, (float) imageBitmap.getWidth() / 2, (float) imageBitmap.getHeight() / 2);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(imageBitmap, 0, 0, bounds.outWidth, bounds.outHeight, matrix, true);
+
+        // Return resulting image
+        return rotatedBitmap;
     }
 }
