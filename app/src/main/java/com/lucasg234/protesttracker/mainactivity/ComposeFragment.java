7c2b5f0535ec5fe95151f3e4b0ca3832cb27a7f8
LucasG234 <lucasgates234@gmail.com>
2020-07-14 12:22:12 -0500

    Added text compose functionality

--- app/src/main/java/com/lucasg234/protesttracker/mainactivity/ComposeFragment.java
+++ app/src/main/java/com/lucasg234/protesttracker/mainactivity/ComposeFragment.java
@@ -1,65 +1,87 @@
 package com.lucasg234.protesttracker.mainactivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.lucasg234.protesttracker.R;
+import com.lucasg234.protesttracker.databinding.FragmentComposeBinding;
+import com.lucasg234.protesttracker.models.Post;
+import com.lucasg234.protesttracker.models.User;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link ComposeFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Fragment where user can create posts
+ * Saves posts to Parse
  */
 public class ComposeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "ComposeFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FragmentComposeBinding mBinding;
 
     public ComposeFragment() {
         // Required empty public constructor
     }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComposeFragment.
+     * Use this factory method to create a new instance of ComposeFragment
      */
-    // TODO: Rename and change types and number of parameters
-    public static ComposeFragment newInstance(String param1, String param2) {
-        ComposeFragment fragment = new ComposeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static ComposeFragment newInstance() {
+        return new ComposeFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compose, container, false);
+        return FragmentComposeBinding.inflate(inflater, container, false).getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mBinding = FragmentComposeBinding.bind(view);
+
+        mBinding.buttonSubmitPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePost();
+            }
+        });
+    }
+
+    // Constructs Post object and saves it to the Parse server
+    private void savePost() {
+        Post post = new Post();
+        post.setText(mBinding.composeEditText.getText().toString());
+        post.setAuthor((User) User.getCurrentUser());
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Log.e(TAG, "Error saving post", e);
+                    Toast.makeText(getContext(), getString(R.string.error_save), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                Log.i(TAG, "Saved post successfully");
+                mBinding.composeEditText.setText("");
+            }
+        });
     }
 }
