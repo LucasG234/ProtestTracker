74073e30d3e851a9d77ea802aa5249508167d9c4
LucasG234 <lucasgates234@gmail.com>
2020-08-04 15:17:12 -0500

    Added loading indiciation to ActionBar

--- app/src/main/java/com/lucasg234/protesttracker/mainactivity/MainActivity.java
+++ app/src/main/java/com/lucasg234/protesttracker/mainactivity/MainActivity.java
@@ -3,6 +3,7 @@ package com.lucasg234.protesttracker.mainactivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
@@ -241,6 +242,13 @@ public class MainActivity extends AppCompatActivity {
                 mMap.ignorePost(post);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_progress_bar, menu);
+        return true;
     }
 
     public void addProcess() {
